7725da4049f059bc181b7de3803b85f21ffe0d55
Aaron Janke <aaronljanke@gmail.com>
2017-04-12 16:21:11 -0500

    fixed s3 issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,13 +52,13 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.paperclip_defaults = {
+config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
-    bucket: ENV.fetch('railsdrums'),
-    access_key_id: ENV.fetch('AKIAJF6RJ5NFKFJWFV2Q'),
-    secret_access_key: ENV.fetch('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'),
-    s3_region: ENV.fetch('US East'),
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
   }
 }
 end
