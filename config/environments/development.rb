b12d26e8a9a294b9b83e9d35b38e94ffb0289d5b
Aaron Janke <aaronljanke@gmail.com>
2017-04-12 16:43:56 -0500

    fixed?

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
 config.paperclip_defaults = {
+  s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
   storage: :s3,
   s3_credentials: {
     bucket: ENV.fetch('S3_BUCKET_NAME'),
